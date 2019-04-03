41889b376c941a9bd29c2176fcda0dbd7dceaf99
unknown <umerderr@gmail.com>
2019-04-03 14:22:34 +0100

    added signup, login and create_days to db

--- database/db.py
+++ database/db.py
@@ -1,17 +1,64 @@
 import sqlite3
+from passlib.hash import sha256_crypt
+from datetime import date
+def connect_db():
+    return sqlite3.connect('pibook.db')
 
-conn = sqlite3.connect('notebook.db')
-
+def response(success,message):
+    return {
+        "success":success,
+        "message":message
+    }
 def create_table(schema):
+    conn = connect_db()
     c = conn.cursor()
     c.execute("PRAGMA foreign_keys = ON;")
     with open(schema,'r') as file:
-	    queries = file.read().split(".");
-	    for query in queries:
-		    c.execute(query)
+		    c.executescript(file.read())
+    conn.commit()
 
-def make_query(query):
-	return conn.cursor().execute(query)
+def create_days():
+    print('creating days')
+    conn = connect_db()
+    c = conn.cursor()
+    c.execute('SELECT userid from User')
+    userids = c.fetchall()
+    d = str(date.today())
+    print(userids)
+    for id in userids:
+        c.execute('INSERT INTO Day VALUES (NULL,?,?)',[d,id[0] ])
+    conn.commit()
 
+def signup_user(username,password,email):
+    conn = connect_db()
+    c = conn.cursor()
+    # check if user with name already exists
+    c.execute("SELECT * FROM User where username = ?",[username])
+    users = c.fetchall() 
+    # if a user without that username exists , add it to the database
+    if not users:
+        hashed_password = sha256_crypt.encrypt(password)
+        c.execute("INSERT INTO User VALUES (NULL,?,?,?)",[username,hashed_password,email])
+        conn.commit()
+        return True
+    else:
+        return False
+
+def login_user(username,try_password):
+    conn = connect_db()
+    c = conn.cursor()
+    c.execute("SELECT password from USER where username = ?",[username])
+    correct_password = c.fetchone()
+    if correct_password:
+        if sha256_crypt.verify(try_password,correct_password):
+            return response(True,'Logged in')
+        else:
+            return response(False,'Wrong password')
+    else:
+        return response(False,'Wrong username')
+
+        
+
+    
 create_table("schema")
 
--- server.py
+++ server.py
@@ -1,8 +1,18 @@
-from flask import Flask
-from database.db import make_query
+from flask import Flask,request,jsonify
+from database.db import signup_user,login_user,create_days
 app = Flask(__name__)
 
-print(make_query("SELECT * FROM User"))
 @app.route('/')
 def hello():
     return "Hello"
+
+@app.route('/signup/', methods=["POST"])
+def signup():
+    if request.method == "POST":
+        data = request.get_json()
+        result = signup_user(data['username'],data['password'],data['email'])
+        return result
+
+signup_user('bob','poop','bob@gmail.com')
+signup_user('steve','poopdescoop','mike@gmail.com')
+create_days()
